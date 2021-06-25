72b29b87b8d238963bfb8bbf9497baa916924420
Sreekumar <73136964+sakhaavvaavaj93@users.noreply.github.com>
2021-06-26 03:51:21 +0530

    Add files via upload

---
+++ loosiebot/modules/__musicals.py
@@ -0,0 +1,7 @@
+__mod_name__ = "MUSIC"
+
+__help__ = """
+ ❍ /audio <song name>*:* Uploads the Audio in it's best quality available
+ 💡Ex: `/song Faded Alan Walker`
+ ❍ /saavn <song name>*:* Download song from saavn.
+"""
---
+++ loosiebot/modules/musicplayer.py
@@ -0,0 +1,39 @@
+
+__mod_name__ = "VC PLAYER"
+__help__ = """
+          》** Loosie_VCPLAYER ** 《
+            
+ TIANA_VCPLAYERplays music in your group's voice chat
+ 
+ Assistant name >> ꧁ @TIANA_VCMUSIC_PLUGIN ꧂
+ 
+⚙️Setting up 
+1) Make bot admin
+2) Start a voice chat
+3) Try `/play [song name]` for the first time by an admin
+*) If userbot joined enjoy music, If not add @TIANA_VCMUSIC_PLUGIN to your group and retry
+  Commands
+
+=>> Song Playing 🎶
+  /play: Play song using youtube music
+  /play [yt url] : Play the given yt url
+  /dplay: Play song via deezer
+  /splay: Play song via jio saavn
+  
+=>> Playback ⏯
+  /player: Open Settings menu of player
+  /skip: Skips the current track
+  /pause: Pause track
+  /resume: Resumes the paused track
+  /end: Stops media playback
+  /current: Shows the current Playing track
+  /playlist: Shows playlist
+
+=>> More tools 📲
+  /admincache: Updates admin info of your group. Try if bot isn't recognize admin
+  /userbotjoin: Invite @TIANA_VCMUSIC_PLUGIN Userbot to your chat
+
+*Player cmd and all other cmds except /play, /current  and /playlist are only for admins with manage group
+
+PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME."""
+
